9900a824d83aeabe0c0a7ef25cd5ac50dea15576
smukhitov <smukhitov@gmail.com>
2023-03-15 18:32:11 -0400

    add comments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# first_streamlit_app
+# This is an application that utilizes streamlit and pandas packages
+# To access it use the following URL:
+https://smukhitov-first-streamlit-app-streamlit-app-qe567d.streamlit.app/#healthy-app 
--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header('Fruity Vice Fruit Advice')
+
+# Creates a text input box where fruit name is entered
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
 
