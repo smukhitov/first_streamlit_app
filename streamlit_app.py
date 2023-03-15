dde83ebc7b01359e8c26b02249bbbcffa14b1418
smukhitov <smukhitov@gmail.com>
2023-03-15 13:22:03 -0400

    add df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 st.title("Hello from my App!")
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-st.text("BS")
+st.dataframe(my_fruit_list)
