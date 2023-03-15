41fef680905a4f646c400e8d476e84fb5e719302
smukhitov <smukhitov@gmail.com>
2023-03-15 18:24:29 -0400

    add input box

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 
+#reading file from AWS S3 bucket
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -12,6 +13,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header('Fruity Vice Fruit Advice')
-fruity_vice_response = requests.get('https://fruityvice.com/api/fruit/kiwi')
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
+#maiking API call get JSON and normalize it in df 
+fruity_vice_response = requests.get('https://fruityvice.com/api/fruit/' + str(fruit_choice))
 fruity_normalized = pd.json_normalize(fruity_vice_response.json())
 st.dataframe(fruity_normalized)
