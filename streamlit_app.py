186aa6133add3a883292f9aaac15cc005c8e3e76
smukhitov <smukhitov@gmail.com>
2023-03-15 13:29:32 -0400

    add filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-st.title("Hello from my App!")
+st.title("Healthy App!")
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.multiselect("Choose some fruits: ", list(my_fruit_list.index))
 st.dataframe(my_fruit_list)
